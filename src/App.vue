d055d1741d3d1dfaed5c5844494540ba1aa5a52e
kevinli8831 <kevinli8831@Gmail.com>
2022-01-20 16:48:39 +0800

    add nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div>
+    <!-- Nav bar -->
+    <nav class="navbar navbar-dark bg-primary justify-content-between flex-nowrap flex-row">
+      <div class="container">
+        <a class="navbar-brand float-left">MEVN Stack Example | LaravelCode</a>
+        <ul class="nav navbar-nav flex-row float-right">
+          <li class="nav-item mx-3">
+            <router-link class="nav-link pr-3" to="/">Create Student</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/view">View Students</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- Router view -->
+    <div class="container mt-5">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
